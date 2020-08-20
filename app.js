832cd7670e33474049d6bd411fad32634f611367
ta-na214 <68981037+ta-na214@users.noreply.github.com>
2020-08-20 11:16:01 +0900

    initial commit
    練習

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'nakamu490',
+  database: 'list_app'
+});
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
+});
+
+app.get('/', (req, res) => {
+  connection.query(
+    'SELECT * FROM users',
+    (error, results) => {
+      console.log(results);
+      res.render('hello.ejs', {users: results, title: 'EXPRESS'});
+    }
+  );
+});
+
+
+
+app.listen(3000);
